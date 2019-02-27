9e6fc7af10ec67445a1bb5e6d2d36788f9697969
Adithya Mukund <adithyamukund@msn.com>
2019-02-27 16:59:31 -0500

    Double clikcing login problem

--- SaaSiOS/LoginViewController.swift
+++ SaaSiOS/LoginViewController.swift
@@ -22,7 +22,33 @@ class LoginViewController: UIViewController {
     
     @IBAction func completeLogin(_ sender: UIButton) {
         Auth.auth().signIn(withEmail: loginEmail.text!, password: loginPassword.text!) { (user, error) in
+            let isLoginSuccessful = error == nil && user != nil
+            
+            if !isLoginSuccessful
+            {
+                let alert = UIAlertController(title: "Login Error", message: error?.localizedDescription, preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
+                    NSLog("The \"OK\" alert occured.")
+                }))
+                self.present(alert, animated: true, completion: nil)
+            }
         }
+        
+        if shouldPerformSegue(withIdentifier: "completeLoginSegue", sender: sender)
+        {
+            print("Segue Succeeded")
+        }
+    }
+    
+    
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        if identifier == "completeLoginSegue"
+        {
+            return Auth.auth().currentUser != nil
+        }
+        
+        return true
     }
     
     /*
