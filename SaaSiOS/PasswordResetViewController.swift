09c7457d702f30fd2470ed0d31c30eb38901b0ae
Adithya Mukund <adithyamukund@msn.com>
2019-02-26 23:21:14 -0500

    Added passwrod reset

---
+++ SaaSiOS/PasswordResetViewController.swift
@@ -0,0 +1,39 @@
+//
+//  PasswordResetViewController.swift
+//  SaaSiOS
+//
+//  Created by Adithya Mukund on 2/26/19.
+//  Copyright © 2019 Adithya Mukund. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+
+class PasswordResetViewController: UIViewController {
+
+    
+    @IBOutlet weak var passwordResetEmail: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func resetPassword(_ sender: Any) {
+        Auth.auth().sendPasswordReset(withEmail: passwordResetEmail.text!) { error in
+
+        }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
