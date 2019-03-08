7e4f990cabc6d19649a3c6f014086fff1041e65a
Adithya Mukund <adithyamukund@msn.com>
2019-03-08 12:16:18 -0500

    Study info page populated

--- SaaSiOS/Controller/StudyInformationViewController.swift
+++ SaaSiOS/Controller/StudyInformationViewController.swift
@@ -9,13 +9,34 @@
 import UIKit
 
 class StudyInformationViewController: UIViewController {
-    
     private var study: Study? = nil
-
+    
+    
+    @IBOutlet weak var studyName: UILabel!
+    @IBOutlet weak var studyDescription: UILabel!
+    @IBOutlet weak var ownerName: UILabel!
+    @IBOutlet weak var ownerJobTitle: UILabel!
+    @IBOutlet weak var ownerEmail: UILabel!
+    @IBOutlet weak var ownerAffiliation: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        studyName.text = study?.getStudyName()
+        studyDescription.text = study?.getDescription()
+        
+        let owner = study?.getOwner()
+        
+        let ownerFirstName = owner?.getFirstName()
+        let ownerLastName = owner?.getLastName()
+        
+        ownerName.text = ownerFirstName
+        ownerName.text!.append(" ")
+        ownerName.text!.append(ownerLastName!)
+        
+        ownerJobTitle.text = owner?.getJobTitle()
+        ownerEmail.text = owner?.getEmail()
+        ownerAffiliation.text = owner?.getAffiliation()
     }
     
     func setStudy(study: Study) -> Void {
